14f3b474329bc2decf0ac1d7c743b745d08269da
Paul Marunko <marunkopavlo@gmail.com>
2024-04-22 18:45:31 -0600

    f2

--- Scratch AI and ML/ML practice/intro.py
+++ Scratch AI and ML/ML practice/intro.py
@@ -7,3 +7,16 @@ def doIt():
     
 
 doIt()
+
+a = [1,2,3,45,6,7,8,9,]
+
+for i in a:
+    
+    for j in len(a)-1:
+        if(a[j] > a[j+1]):
+            temp = a[j]
+            a[j] = a[j+1]
+            a[j+1] = temp
+
+print(a)
+
